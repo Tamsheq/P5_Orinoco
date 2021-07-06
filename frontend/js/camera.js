9822bb0158a7381c629e157da3497b2ea01b1fba
Raphael Abdiche <abdiche.raphael@gmail.com>
2021-07-06 18:59:05 +0200

    index presque finit

--- frontend/js/camera.js
+++ frontend/js/camera.js
@@ -2,10 +2,4 @@ class Camera {
     constructor(jsonCamera) {
         jsonCamera && Object.assign(this, jsonCamera);
     }
-
-    getFormatedDate(camera) {
-        let timestamp = Date.parse(publicationDate);
-        let date = new Date(timestamp);
-        return date.toLocaleDateString();
-    }
 }
--- frontend/view/index/index.js
+++ frontend/view/index/index.js
@@ -2,18 +2,19 @@ loadConfig().then(data => {
     config = data;
     fetch(config.host + "/api/cameras").then(data => data.json())
         .then(jsonAllCameras => {
-            for (let jsonCameras of JsonAllCameras) {
-                let cameras = new Cameras(jsonCameras);
+            for (let jsonCameras of jsonAllCameras) {
+                let cameras = new Camera(jsonCameras);
                 document.querySelector(".container").innerHTML += `<div class="col-12 mt-5">
                                                                         <div class="card cameras">
                                                                             <div class="card-header">
-                                                                                <h5 class="card-title d-flex justify-content-between">${cameras.name}<span class="all-cameras">${cameras.getAllCameras()}</span></h5>    
+                                                                                <h5 class="card-title d-flex justify-content-between">${cameras.name}</h5>    
                                                                             </div>
-                                                                            <img src="${config.host}/${cameras.imageUrl}" class="card-img-top">
-                                                                                <span class="fa-stack fa-2x addPannier" data-id=${cameras.id}>
+                                                                            <img src="${cameras.imageUrl}" class="card-img-top">
+                                                                                <span class="fa-stack fa-2x addPannier" data-id=${cameras._id}>
                                                                                 </span>
                                                                                 <div class="card-body">
-                                                                                    <p class="card-text">${cameras.content}</p>
+                                                                                    <p class="card-price">${cameras.price / 100} &#8364;</p>
+                                                                                    <p class="card-text">${cameras.description}</p>
                                                                                     </div>
                                                                                 </div>
                                                                             </div>
