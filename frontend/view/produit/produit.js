b6197c59c4073e29f57a2d8e905284cf1b48ab83
Raphael Abdiche <abdiche.raphael@gmail.com>
2021-07-10 17:12:37 +0200

    panier.html et panier.js en cours , doit améliorer produit.js

--- frontend/view/produit/produit.js
+++ frontend/view/produit/produit.js
@@ -3,11 +3,9 @@ const urlParams = new URLSearchParams(query);
 const id = urlParams.get('toto');
 
 loadConfig().then(data => {
-    console.log('toto', data)
     config = data;
     fetch(`${config.host}/api/cameras/${id}`).then(data => data.json())
         .then(response => {
-            console.log(response)
             document.querySelector(".container").innerHTML += `<h1 class="row">${response.name}</h1>
                 <p class="row"><img src="${response.imageUrl}" alt="image de l'appareil" style="width:90%; border-radius:5px;"></p>
                 <p class="row">${response.description}</p>
@@ -22,11 +20,13 @@ loadConfig().then(data => {
                             <button class="addPanier btn btn-info justify-content-center mx-auto"><b>Ajouter au panier</b><i class="fas fa-cart-arrow-down"></i></button>`;
 
             function addLenses(product) {
-                const versionChoice = document.getElementById("option");
+                const versionChoice = document.getElementsByClassName("section__choice")[0];
                 for (let lenses of product.lenses) {
+                    console.log(lenses)
                     versionChoice.innerHTML += `<option value="${lenses}">${lenses}</option>`;
                 }
             }
+            addLenses(response)
         });
 });
 /* <p class="card-text">${cameras.description}</p> */
