b44e7c15308e02dd4d4972005c2ef314b226344c
Raphael Abdiche <abdiche.raphael@gmail.com>
2021-07-12 02:35:54 +0200

    panier.js / html en cours

---
+++ frontend/view/panier/panier.js
@@ -0,0 +1,41 @@
+const orderForm = document.getElementById("orderForm");
+const emptyBag = document.getElementById("emptyBag");
+
+// Si le panier est vide
+if (emptyBag.lenght < 1) {
+    orderForm.classList.add("d-none");
+
+    // Sinon affichage des produits
+} else {
+    orderForm.classList.add("d-none");
+    emptyBag.classList.add("d-none");
+
+    const fullBag = document.getElementById("bag");
+    fullBag.classList.toggle("d-none");
+    for (product of basket) {
+        displayProductionListTable(product);
+    }
+
+    // Ajout du produit
+    function addProduct(event) {
+        const index = event.target.getAttribute("data-index");
+        bag[index].quantity++;
+        localStorage.setItem("cameras", JSON.stringify(basket));
+        location.reload();
+    }
+
+    const buttonAdd = document.getElementsByClassName("plus");
+    for (add of buttonAdd) {
+        add.addEventListenet("click", addProduct);
+    }
+
+    // Prix total
+    totalPrice();
+
+    // Vider le panier
+    const buttonClearBAG = document.getElementById("clearBag");
+    buttonClearBAG.addEventListener("click", () => {
+        clearBag();
+        location.reload();
+    })
+}
